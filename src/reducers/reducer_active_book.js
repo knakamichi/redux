cb03ff5eb5f6d5d1f25a889ab0a97f551652b7e1
kokezeror <kokezeror@yahoo.co.jp>
2018-08-06 16:16:28 +0900

    test

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,14 @@
+// State argument is not an application state, only the state this reducer is responsible for.
+// state=null -> if the state argument is undefined, define it as null (es6 statement)
+export default function (state = null, action) {
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+
+      // don`t mutate state! ex bad code below
+      // state.title = book.title
+      // return state
+  }
+
+  return state;
+}
